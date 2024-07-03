6a908d0bacf06959612948e9830e09fd69a1deec
Chellsea Robinson <60821695+chellseacarmel@users.noreply.github.com>
2024-07-03 18:53:38 -0400

    Initial Commit

---
+++ Desserts-Gallore-y/Desserts_Gallore_yApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desserts_Gallore_yApp.swift
+//  Desserts-Gallore-y
+//
+//  Created by Chellsea Carmel on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Desserts_Gallore_yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
