d25949984d8a3419e90c4421717af99a70d33bb9
Chellsea Robinson <60821695+chellseacarmel@users.noreply.github.com>
2024-07-09 18:42:57 -0400

    Add unit tests.

--- Desserts-Gallore-yTests/Desserts_Gallore_yTests.swift
+++ Desserts-Gallore-yTests/Desserts_Gallore_yTests.swift
@@ -6,30 +6,46 @@
 //
 
 import XCTest
+@testable import Desserts_Gallore_y
+
 
 final class Desserts_Gallore_yTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    /// Function to check if the result from fetchDesserts is not empty.
+    func test_fetch_desserts_result() async throws {
+        let desserts = try await MealService.shared.fetchDesserts()
+        
+        XCTAssertFalse(desserts.isEmpty,"Desserts array should not be empty")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    /// Fucntion to check if the result from fetchDessertDetail for a vaild id is not Empty.
+    func test_fetch_desserts_detailed_valid() async throws {
+        let dessertDetail = try await MealService.shared.fetchDessertDetail(by: "52853")
+        
+        XCTAssertNotNil(dessertDetail,"DessertDetails should not be empty")
     }
+    
+    /// Fucntion to check if the result from fetchDessertDetail for an invaild id throws an Error.
+    func test_fetch_desserts_detailed_invalid() async throws {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+        do {
+            _ = try await MealService.shared.fetchDessertDetail(by: "0")
+            XCTFail("Expected fetchDesertDetail to throw an error for invalid id")
+        }catch{
+            XCTAssert(true, "Error was thrown as expected");
         }
+        
     }
+    
+    /// Fucntion to check if the result from fetchDessertDetail for an empty id throws an Error.
+    func test_fetch_desserts_detailed_empty() async throws {
 
+        do {
+            _ = try await MealService.shared.fetchDessertDetail(by: "")
+            XCTFail("Expected fetchDesertDetail to throw an error for an empty id")
+        }catch{
+            XCTAssert(true, "Error was thrown as expected");
+        }
+        
+    }
 }
